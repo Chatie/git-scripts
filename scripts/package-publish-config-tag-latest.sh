5351de0a6986885ac03829c6b1b1601c1302d851
Huan LI <zixia@zixia.net>
2019-06-10 18:05:12 +0800

    fix npx & ci

--- scripts/package-publish-config-tag-latest.sh
+++ scripts/package-publish-config-tag-latest.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Bug compatible with -r not supported (we need a raw string)
-VERSION=$(ts-node --files bin/pkg-jq -r .version)
+VERSION=$(npx pkg-jq -r .version)
 
-if npx semver-is-prod $VERSION; then
+if npx --project @chatie/semver semver-is-prod $VERSION; then
   npx pkg-jq -i '.publishConfig.tag="latest"'
   echo "production release: publicConfig.tag set to latest."
 else
