1dcd6b8d1ca21bec27f542277a82e64116513170
Huan LI <zixia@zixia.net>
2019-06-10 17:33:05 +0800

    fix pack testing

---
+++ bin/install.ts
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+import fs from 'fs'
+import path from 'path'
+import util from 'util'
+
+import pkgUp from 'pkg-up'
+
+interface PackageGitScripts {
+  git?: {
+    scripts?: {
+      'pre-push'?: string
+    }
+  }
+}
+
+async function main (): Promise<number> {
+  const cwd = path.join(
+    __dirname,  // bin
+    '..',       // dist
+    '..',       // root
+    '..',       // @chatie
+  )
+
+  const pkgFile = await pkgUp({ cwd })
+
+  if (!pkgFile) {
+    console.error('@chatie/git-scripts: can not find any package.json')
+    return 1
+  }
+
+  const pkg = require(pkgFile) as PackageGitScripts
+
+  if (pkg && pkg.git && pkg.git.scripts && pkg.git.scripts['pre-push']) {
+    // there's a existing pre-push value
+    return 0
+  }
+
+  pkg.git = pkg.git || {}
+
+  pkg.git.scripts = {
+    ...pkg.git.scripts,
+    'pre-push': 'git-scripts-pre-push',
+  }
+
+  const jsonText = JSON.stringify(pkg, null, 2)
+  const promiseWriteFile = util.promisify(fs.writeFile)
+
+  await promiseWriteFile(pkgFile, jsonText)
+
+  return 0
+}
+
+main()
+  .then(process.exit)
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
