d0c533d443c770adc93c236ae905f7500ee669da
Huan LI <zixia@zixia.net>
2020-03-19 18:27:07 +0800

    fix install script permission problem

--- scripts/install.js
+++ scripts/install.js
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
 const fs = require('fs')
-const { exec } = require('child_process')
+const path = require('path')
 
-const INSTALLJS = 'dist/bin/install.js'
+const INSTALLJS = path.join(
+  __dirname,
+  '..',
+  'dist',
+  'bin',
+  'install.js',
+)
 
 function main () {
   if (fs.existsSync(INSTALLJS)) {
-    exec(INSTALLJS, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`)
-        return
-      }
-      console.info(stdout)
-      if (stderr) {
-        console.error(stderr)
-      }
-    })
+    require(INSTALLJS)
   } else {
     console.info(`@chatie/git-scripts postinstall ${INSTALLJS} not found`)
   }
